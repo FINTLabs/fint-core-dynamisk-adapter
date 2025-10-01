cac894e61ca38018d776050c77270731772b7552
sindre <sindre.giske@novari.no>
2025-10-01 13:00:31 +0200

    UI Progress

--- src/main/kotlin/no/fintlabs/dynamiskadapter/Main.kt
+++ src/main/kotlin/no/fintlabs/dynamiskadapter/Main.kt
@@ -1,7 +1,9 @@
 package no.fintlabs.dynamiskadapter
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +13,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -18,14 +21,24 @@ import androidx.compose.ui.window.application
 fun main() =
     application {
         Window(onCloseRequest = ::exitApplication, title = "FINT Dynamisk Adapter") {
-            app()
+            configMenu()
         }
     }
 
 @Composable
 @Preview
-fun app() {
-    var orgId by remember { mutableStateOf<String>("fint.no") }
+fun configMenu() {
+    var orgId by remember { mutableStateOf<String>("fint-no") }
+    var amountOfResources by remember { mutableStateOf<String>("4") }
+    var domainContext by remember { mutableStateOf<String>("fint-core") }
+    var selectedResource by remember { mutableStateOf<String>("utdanning-elev") }
+    var resourceMenuOpen by remember { mutableStateOf<Boolean>(false) }
+    var currentErrorMessage by remember { mutableStateOf<String?>(null) }
+    val resourceOptionList =
+        listOf(
+            "utdanning-elev",
+            "utdanning-elevfravar",
+        )
 
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
@@ -34,10 +47,10 @@ fun app() {
         Text(
             text = "FINT Dynamisk Adapter",
             modifier = Modifier.padding(16.dp),
-            style = MaterialTheme.typography.h5,
+            style = MaterialTheme.typography.h4,
         )
-        Text(text = "Denne tjenesten lager Kafka Topics du kan utnytte til lokal texting av tjenester under utvikling.")
-        Text(text = "")
+        Text(text = "Denne tjenesten lager Kafka Topics du kan utnytte til lokal testing av tjenester under utvikling.")
+        Text(text = "Fyll inn ønskede dataparameter under. Om du ikke velger blir default verdiene brukt.")
         Row(
             modifier = Modifier.fillMaxWidth().weight(1f),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -46,23 +59,71 @@ fun app() {
                 modifier = Modifier.fillMaxWidth().weight(1f),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text(text = "Kafka configuration:")
+                Text(
+                    text = "Kafka configuration:",
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.h5,
+                )
                 TextField(
                     value = orgId,
                     onValueChange = { orgId = it },
+                    label = { Text("orgId:") },
                 )
-
-                Text(text = "Data configuration:")
                 TextField(
-                    value = orgId,
-                    onValueChange = { orgId = it },
-                    label = { Text("orgId") },
+                    value = domainContext,
+                    onValueChange = { domainContext = it },
+                    label = { Text("domainContext:") },
                 )
+                TextField(
+                    value = amountOfResources,
+                    onValueChange = { amountOfResources = it },
+                    label = { Text("Antall ønskede Resurser") },
+                )
+                Text("Selected Resource: ")
+                Text(selectedResource, style = MaterialTheme.typography.h6)
+                Button(onClick = { resourceMenuOpen = !resourceMenuOpen }) {
+                    Text("select another resource")
+                }
+                DropdownMenu(
+                    expanded = resourceMenuOpen,
+                    onDismissRequest = { resourceMenuOpen = false },
+                ) {
+                    resourceOptionList.forEach { resourceOption ->
+                        DropdownMenuItem(onClick = {
+                            selectedResource = resourceOption
+                            resourceMenuOpen = false
+                        }) {
+                            Text(resourceOption)
+                        }
+                    }
+                }
             }
             Column(
-                modifier = Modifier.fillMaxWidth().weight(1f),
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
+                if (currentErrorMessage != null) {
+                    Box(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .border(2.dp, Color.Red)
+                                .padding(16.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            Text("ERROR:", style = MaterialTheme.typography.h6)
+                            Text(currentErrorMessage!!)
+                        }
+                    }
+                }
             }
         }
     }
