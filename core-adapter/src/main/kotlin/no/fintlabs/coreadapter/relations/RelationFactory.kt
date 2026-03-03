11c535c2120ec8f5d784c2cfa9cfd65244698bcd
sindre <sindre.giske@novari.no>
2026-03-03 15:47:46 +0100

    core-adapter: major RelationFactory refactor

--- core-adapter/src/main/kotlin/no/fintlabs/coreadapter/relations/RelationFactory.kt
+++ core-adapter/src/main/kotlin/no/fintlabs/coreadapter/relations/RelationFactory.kt
@@ -7,6 +7,7 @@ import no.fintlabs.coreadapter.data.ExpandedMetadata
 import no.fintlabs.coreadapter.store.ResourceStore
 import no.fintlabs.coreadapter.util.putLink
 import no.fintlabs.coreadapter.util.toResourceKey
+import no.fintlabs.metamodel.MetamodelService
 import org.springframework.stereotype.Component
 
 private data class Edge(
@@ -14,9 +15,15 @@ private data class Edge(
     val to: String,
 )
 
+private enum class LinkRule {
+    ROUND_ROBIN,
+    UNIQUE_STRICT,
+}
+
 @Component
 class RelationFactory(
     private val props: DynamicAdapterProperties,
+    private val model: MetamodelService,
     private val storage: ResourceStore,
 ) {
     // TODO: REFACTOR AFTER STORE CHANGE
@@ -26,65 +33,96 @@ class RelationFactory(
         for (resource in metadataList) {
             val relations: List<FintRelation> = resource.resource.relations
             for (relation in relations) {
-                if (relation.multiplicity == FintMultiplicity.NONE_TO_ONE ||
-                    relation.multiplicity == FintMultiplicity.NONE_TO_MANY
-                ) {
+                val edgeToSkip = Edge(relation.toResourceKey(), resource.key)
+                if (edgeToSkip in skip) {
+                    logIfEnabled("⛓️ ${resource.resource.name} Link skipped for ${relation.name}")
                     continue
                 } else {
-                    val edgeToSkip = Edge(relation.toResourceKey(), resource.key)
-                    if (edgeToSkip in skip) {
-                        logIfEnabled("⛓️ ${resource.resource.name} Link skipped for ${relation.name}")
-                        continue
-                    } else {
-                        val secondaryMetadata: ExpandedMetadata? =
-                            metadataList.firstOrNull { it.key == relation.toResourceKey() }
-                        if (secondaryMetadata != null) {
-                            val reverseRelation =
-                                secondaryMetadata.resource.relations.firstOrNull {
-                                    it.toResourceKey() == resource.key
+                    when (relation.multiplicity) {
+                        FintMultiplicity.NONE_TO_MANY,
+                        FintMultiplicity.NONE_TO_ONE,
+                        -> {
+                            continue
+                        }
+
+                        FintMultiplicity.ONE_TO_MANY -> {
+                            val secondaryMetadata = getSecondaryMetadata(relation)
+                            if (secondaryMetadata != null) {
+                                val secondaryMultiplicity = getSecondaryMultiplicity(resource.key, secondaryMetadata)
+                                if (secondaryMultiplicity == FintMultiplicity.ONE_TO_MANY) {
+                                    giveLink(resource.key, relation, LinkRule.ROUND_ROBIN)
+                                    skip.add(Edge(resource.key, relation.toResourceKey()))
                                 }
-                            val secondaryMultiplicity = reverseRelation?.multiplicity
-                            if (secondaryMultiplicity == FintMultiplicity.ONE_TO_ONE &&
-                                relation.multiplicity == FintMultiplicity.ONE_TO_MANY
-                            ) {
-                                continue
-                            } else {
-                                val primaryIds = storage.getAll(resource.key).map { it.id }
-                                val secondaryIds = storage.getAll(secondaryMetadata.key).map { it.id }
-
-                                primaryIds.forEachIndexed { i, primaryId ->
-                                    val target =
-                                        if (secondaryMultiplicity == FintMultiplicity.ONE_TO_ONE && i >= secondaryIds.size) {
-                                            "NOT_ENOUGH_${relation.name}"
-                                        } else {
-                                            secondaryIds[i % secondaryIds.size]
-                                        }
-
-                                    storage.updateResource(resource.key, primaryId) { r ->
-                                        r.putLink(relation.name, target)
-                                    }
-                                }
-                                skip.add(Edge(resource.key, relation.toResourceKey()))
-
-                                logIfEnabled("⛓️ ${resource.resource.name} now has links to ${relation.name}")
-                                logIfEnabled("")
                             }
-                        } else {
-                            if (relation.multiplicity == FintMultiplicity.ONE_TO_ONE) {
-                                logIfEnabled("")
-                                logIfEnabled("⚠️ ${resource.resource.name}'s required relation ${relation.name} not found in localStorage.")
-                                logIfEnabled("Add ${relation.packageName} to initialDataSets.")
-                                logIfEnabled("")
-                            } else {
-                                continue
+                        }
+
+                        FintMultiplicity.ONE_TO_ONE -> {
+                            val secondaryMetadata = getSecondaryMetadata(relation)
+                            if (secondaryMetadata != null) {
+                                val secondaryMultiplicity = getSecondaryMultiplicity(resource.key, secondaryMetadata)
+                                val linkRule =
+                                    if (secondaryMultiplicity == FintMultiplicity.ONE_TO_ONE) {
+                                        LinkRule.UNIQUE_STRICT
+                                    } else {
+                                        LinkRule.ROUND_ROBIN
+                                    }
+                                giveLink(resource.key, relation, linkRule)
+                                skip.add(Edge(resource.key, relation.toResourceKey()))
                             }
                         }
                     }
                 }
             }
         }
+        println("")
         println("⚙️✅ InitialDataset relating complete.")
     }
+
+    private fun giveLink(
+        primaryKey: String,
+        relation: FintRelation,
+        linkRule: LinkRule,
+    ) {
+        val secondaryKey = relation.toResourceKey()
+        val primaryIds = storage.getAll(primaryKey).map { it.id }
+        val secondaryIds = storage.getAll(secondaryKey).map { it.id }
+
+        primaryIds.forEachIndexed { i, primaryId ->
+            val target =
+                if (linkRule == LinkRule.UNIQUE_STRICT) {
+                    "NOT_ENOUGH_$secondaryKey"
+                } else {
+                    secondaryIds[i % secondaryIds.size]
+                }
+
+            storage.updateResource(primaryKey, primaryId) { r ->
+                r.putLink(relation.name, target)
+            }
+        }
+        logIfEnabled("⛓️ $primaryKey now has links to $secondaryKey")
+    }
+
+    private fun getSecondaryMetadata(relation: FintRelation): ExpandedMetadata? {
+        val key = relation.toResourceKey()
+        val component = key.substringBeforeLast("/").replace('/', '.')
+        val resource = key.substringAfterLast("/")
+        val resourceData = model.getResource(component, resource)
+        if (resourceData == null) {
+            println("getSecondaryMetadata $component/$resource not found")
+            return null
+        } else {
+            return ExpandedMetadata(resourceData, relation.toResourceKey())
+        }
+    }
+
+    private fun getSecondaryMultiplicity(
+        primaryKey: String,
+        secondaryMetaData: ExpandedMetadata,
+    ): FintMultiplicity? =
+        secondaryMetaData.resource.relations
+            .firstOrNull {
+                it.toResourceKey() == primaryKey
+            }?.multiplicity
 
     private fun logIfEnabled(log: String) {
         if (props.consoleLogging) println(log)
