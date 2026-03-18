2d7324203cc340b7ffc817f688218b036cc913b3
sindre <sindre.giske@novari.no>
2026-03-18 11:15:34 +0100

    core-adapter: tweak

--- core-adapter/src/main/kotlin/no/fintlabs/coreadapter/config/AdapterProperties.kt
+++ core-adapter/src/main/kotlin/no/fintlabs/coreadapter/config/AdapterProperties.kt
@@ -7,16 +7,16 @@ import kotlin.uuid.Uuid
 @ConfigurationProperties(prefix = "fint.adapter")
 data class AdapterProperties(
     private val id: String,
-    val orgId: String = "fintlabs-no",
+    val orgId: String = "fintlabs.no",
     val clientId: String,
     val clientSecret: String,
     val scope: String,
     val username: String,
     val password: String,
-    val baseUrl: String,
     val idpUri: String,
     val heartbeatIntervalInMinutes: Int = 3,
     val capabilities: List<Capability> = emptyList(),
+    val baseUrl: String = "https://beta.felleskomponent.no",
 ) {
     @OptIn(ExperimentalUuidApi::class)
     val adapterId: String = "$id/$${Uuid.random()}"
