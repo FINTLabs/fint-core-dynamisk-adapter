5f21b060b9ccd5fb438ec7bb8361421553ab2705
sindre <sindre.giske@novari.no>
2026-05-19 11:21:12 +0200

    core-lib: Randomizer buff tests to confirm functionality

--- core-lib/src/test/kotlin/LibrarySeedTests.kt
+++ core-lib/src/test/kotlin/LibrarySeedTests.kt
@@ -1,4 +1,5 @@
 import no.fintlabs.library.ResourceFactory
+import no.fintlabs.library.ResourceFactoryConfig
 import no.novari.fint.model.resource.utdanning.elev.ElevResource
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -9,18 +10,18 @@ class LibrarySeedTests {
 
     @Test
     fun `same seed generates same data`() {
-        val firstRun = ResourceFactory(seed = "test-seed")
+        val firstRun = ResourceFactory(config = ResourceFactoryConfig(seed = "test-seed"))
             .create(ElevResource::class.java, 1)
 
-        val secondRun = ResourceFactory(seed = "test-seed")
+        val secondRun = ResourceFactory(config = ResourceFactoryConfig(seed = "test-seed"))
             .create(ElevResource::class.java, 1)
 
         assertEquals(firstRun.toString(), secondRun.toString())
     }
 
     @Test
-    fun `individual generated resources in seed are still random`() {
-        val resources = ResourceFactory(seed = "test-seed")
+    fun `individual generated resources in seed are not equal`() {
+        val resources = ResourceFactory(config = ResourceFactoryConfig(seed = "test-seed"))
             .create(ElevResource::class.java, 2)
 
         assertNotEquals(resources[0].toString(), resources[1].toString())
@@ -28,10 +29,10 @@ class LibrarySeedTests {
 
     @Test
     fun `different seed generates different data`() {
-        val firstRun = ResourceFactory(seed = "seed-one")
+        val firstRun = ResourceFactory(config = ResourceFactoryConfig(seed = "seed-one"))
             .create(ElevResource::class.java, amount = 1)
 
-        val secondRun = ResourceFactory(seed = "seed-two")
+        val secondRun = ResourceFactory(config = ResourceFactoryConfig(seed = "seed-two"))
             .create(ElevResource::class.java, amount = 1)
 
         assertNotEquals(
@@ -42,10 +43,10 @@ class LibrarySeedTests {
 
     @Test
     fun `empty seed generates non deterministic data`() {
-        val firstRun = ResourceFactory(seed = "")
+        val firstRun = ResourceFactory(config = ResourceFactoryConfig(seed = ""))
             .create(ElevResource::class.java, amount = 1)
 
-        val secondRun = ResourceFactory(seed = "")
+        val secondRun = ResourceFactory(config = ResourceFactoryConfig(seed = ""))
             .create(ElevResource::class.java, amount = 1)
 
         assertNotEquals(
@@ -53,4 +54,30 @@ class LibrarySeedTests {
             secondRun.toString(),
         )
     }
+
+    @Test
+    fun `seed does not reset on every run`() {
+        val factory = ResourceFactory(config = ResourceFactoryConfig(seed = "test-seed"))
+
+        val firstRun = factory.create(ElevResource::class.java, amount = 1)
+
+        val secondRun = factory.create(ElevResource::class.java, amount = 1)
+
+        assertNotEquals(firstRun.toString(), secondRun.toString())
+    }
+
+    @Test
+    fun `seed reset generates same data`() {
+        val factory = ResourceFactory(config = ResourceFactoryConfig(seed = "test-seed"))
+
+        val firstRun = factory.create(ElevResource::class.java, amount = 1)
+
+        factory.resetSeed()
+
+        val secondRun = factory.create(ElevResource::class.java, amount = 1)
+
+        assertEquals(firstRun.toString(), secondRun.toString())
+    }
+
+
 }
